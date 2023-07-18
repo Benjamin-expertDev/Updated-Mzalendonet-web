41360ba21a6e5e3a02483bd7a10f0dede252ef54
Benjamin Devexpert <benjaminmwambura@gmail.com>
2023-07-18 15:51:05 +0300

    javascript for pricing

--- pricingcart.js
+++ pricingcart.js
@@ -6,3 +6,26 @@
         // Redirect the user to pricing.html
         window.location.href = 'pricing.html';
     });
+    // Get a reference to the "Buy" link and the cart number span
+    const buyLink = document.getElementById('buyLink');
+    const cartNumberSpan = document.querySelector('.index-1-cart-number-popup span');
+
+    // Initialize the cart number counter
+    let cartNumber = 0;
+
+    // Function to increment the cart number and update the span
+    function incrementCartNumber() {
+        cartNumber++;
+        cartNumberSpan.textContent = cartNumber.toString();
+    }
+
+    // Add a click event listener to the "Buy" link
+    buyLink.addEventListener('click', function(event) {
+        // Prevent the default link behavior (i.e., following the link)
+        event.preventDefault();
+        // Increment the cart number
+        incrementCartNumber();
+        // Redirect the user to the contact.html page
+        window.location.href = 'contact.html';
+    });
+    

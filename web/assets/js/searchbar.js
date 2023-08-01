91c16a6ab73d9959b99266c3b44f6c0ee6897c20
Benjamin Devexpert <benjaminmwambura@gmail.com>
2023-08-01 11:24:19 +0300

    User js redirection

--- web/assets/js/searchbar.js
+++ web/assets/js/searchbar.js
@@ -1,11 +1,15 @@
+
 document.getElementById("search-form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent form submission
 
     // Get the user's search query from the input field
     const searchQuery = document.getElementById("search-input").value;
 
-    // Construct the Google search URL with the search query
-    const googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent(searchQuery);
+   
+    const siteDomain = "http://www.mzalendoafrinet.co.ke/public/";
+
+    // Construct the Google search URL with the site: operator
+    const googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent("site:" + siteDomain + " " + searchQuery);
 
     // Redirect the user to Google with the search query
     window.location.href = googleSearchURL;

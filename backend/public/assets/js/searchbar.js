162c79a4ad917c8a2702537311d2a0101faf2086
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-08-29 14:20:06 +0300

    commit on search-bar

--- backend/public/assets/js/searchbar.js
+++ backend/public/assets/js/searchbar.js
@@ -1,16 +1,15 @@
-
 document.getElementById("search-form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent form submission
 
     // Get the user's search query from the input field
     const searchQuery = document.getElementById("search-input").value;
 
-   //site to changed
+    // Site domain where you want to conduct the search
     const siteDomain = "http://website.mzalendonet.co.ke/";
 
-    // Construct the Google search URL with the site: operator
-    const googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent("site:" + siteDomain + " " + searchQuery);
+    // Construct the site-specific search URL
+    const siteSearchURL = siteDomain + "search?q=" + encodeURIComponent(searchQuery);
 
-    // Redirect the user to Google with the search query
-    window.location.href = googleSearchURL;
+    // Redirect the user to the site-specific search page
+    window.location.href = siteSearchURL;
 });

15bddf55e81a95b7ac17288b8fe1847981d26046
Benjamin-expertDev <benjaminmwambura@gmail.com>
2023-08-29 14:13:34 +0300

    Search-bar

--- backend/public/assets/js/searchbar.js
+++ backend/public/assets/js/searchbar.js
@@ -1,11 +1,15 @@
+
 document.getElementById("search-form").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevent form submission
 
     // Get the user's search query from the input field
     const searchQuery = document.getElementById("search-input").value;
 
-    // Construct the Google search URL with the search query
-    const googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent(searchQuery);
+   //site to changed
+    const siteDomain = "http://website.mzalendonet.co.ke/";
+
+    // Construct the Google search URL with the site: operator
+    const googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent("site:" + siteDomain + " " + searchQuery);
 
     // Redirect the user to Google with the search query
     window.location.href = googleSearchURL;
--- web/assets/js/searchbar.js
+++ web/assets/js/searchbar.js
@@ -6,7 +6,7 @@ document.getElementById("search-form").addEventListener("submit", function(event
     const searchQuery = document.getElementById("search-input").value;
 
    //site to changed
-    const siteDomain = "http://www.mzalendoafrinet.co.ke/public/";
+    const siteDomain = "http://website.mzalendonet.co.ke/";
 
     // Construct the Google search URL with the site: operator
     const googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent("site:" + siteDomain + " " + searchQuery);
